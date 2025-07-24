29a6af33e4f02d800629852e4c3c9771c3a05c04
Muskankesarwani-erm <muskan.kesarwani@erm.com>
2025-07-24 19:31:13 +0530

    adding cpu util alert

--- main.tf
+++ main.tf
@@ -15,8 +15,8 @@ resource "aws_cloudwatch_metric_alarm" "system_check_alarm" {
   }
 
   alarm_description = "System status check failed for ${each.key}"
-  alarm_actions     = [aws_sns_topic.system_alerts.arn]
-  ok_actions        = [aws_sns_topic.system_alerts.arn]
+  alarm_actions = [aws_sns_topic.ec2_alerts.arn]
+  ok_actions        = [aws_sns_topic.ec2_alerts.arn]
 }
 
 # Instance Check Alarm
@@ -36,28 +36,37 @@ resource "aws_cloudwatch_metric_alarm" "instance_check_alarm" {
   }
 
   alarm_description = "Instance status check failed for ${each.key}"
-  alarm_actions     = [aws_sns_topic.instance_alerts.arn]
-  ok_actions        = [aws_sns_topic.instance_alerts.arn]
+  alarm_actions = [aws_sns_topic.ec2_alerts.arn]
+  ok_actions        = [aws_sns_topic.ec2_alerts.arn]
 }
 
-# System Check SNS
-resource "aws_sns_topic" "system_alerts" {
-  name = "ec2-system-check-alerts"
+resource "aws_cloudwatch_metric_alarm" "cpu_utilization_alarm" {
+  for_each = toset(var.ec2_instance_ids)
+
+  alarm_name          = "HighCPU-${each.value}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = var.evaluation_periods
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = var.period
+  statistic           = "Average"
+  threshold           = var.cpu_threshold
+  alarm_description   = "CPU Utilization > ${var.cpu_threshold}% for ${var.datapoints_to_alarm} out of ${var.evaluation_periods} minutes for instance ${each.value}"
+  alarm_actions = [aws_sns_topic.ec2_alerts.arn]
+  dimensions = {
+    InstanceId = each.value
+  }
+  datapoints_to_alarm = var.datapoints_to_alarm
+  treat_missing_data  = "notBreaching"
 }
 
-resource "aws_sns_topic_subscription" "system_email" {
-  topic_arn = aws_sns_topic.system_alerts.arn
+
+resource "aws_sns_topic" "ec2_alerts" {
+  name = "ec2-alerts"
+}
+
+resource "aws_sns_topic_subscription" "email" {
+  topic_arn = aws_sns_topic.ec2_alerts.arn
   protocol  = "email"
-  endpoint  = var.system_check_email
-}
-
-# Instance Check SNS
-resource "aws_sns_topic" "instance_alerts" {
-  name = "ec2-instance-check-alerts"
-}
-
-resource "aws_sns_topic_subscription" "instance_email" {
-  topic_arn = aws_sns_topic.instance_alerts.arn
-  protocol  = "email"
-  endpoint  = var.instance_check_email
+  endpoint  = var.notification_email
 }
